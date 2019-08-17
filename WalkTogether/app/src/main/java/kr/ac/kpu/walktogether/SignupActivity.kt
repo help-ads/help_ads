3d102eea9ea1da6e604a6ef0292a082ab803e8c7
lightlamp <gkfla1101@gmail.com>
2019-08-17 17:21:04 +0900

    8.17 로그인 수정 RegisterActivity -> SignupActivity 이름변경 회원가입 화면에 사진 추가

--- WalkTogether/app/src/main/java/kr/ac/kpu/walktogether/SignupActivity.kt
+++ WalkTogether/app/src/main/java/kr/ac/kpu/walktogether/SignupActivity.kt
@@ -1,16 +1,19 @@
 package kr.ac.kpu.walktogether
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_signup.*
+import java.util.*
 
 class SignupActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,16 +21,41 @@ class SignupActivity : AppCompatActivity() {
 
         register_button_register.setOnClickListener {
             performRegister()
+
         }
 
         already_have_account_textview.setOnClickListener{
-            Log.d("MainActivity","Try to show login activity")
+            Log.d("RegisterActivity","Try to show login activity")
 
             //launch the login activity somehow
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-
         }
+
+        selectphoto_button_register.setOnClickListener {
+            Log.d("RegisterActivity", "Try to show photo selector")
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+    }
+    var selectedPhotoUri: Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data !=null)
+            Log.d("RegisterActivity", "Photo was selected")
+
+        selectedPhotoUri = data?.data//data.data
+        val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+
+        selectphoto_imageview_register.setImageBitmap(bitmap)
+
+        selectphoto_button_register.alpha = 0f
+//        val bitmapDrawable = BitmapDrawable(bitmap)
+//        selectphoto_button_register.setBackgroundDrawable(bitmapDrawable)
     }
     private fun performRegister(){
         val email = email_edittext_register.text.toString()
@@ -38,33 +66,61 @@ class SignupActivity : AppCompatActivity() {
             return
         }
 
-        Log.d("MainActivity", "Email is: " + email)
-        Log.d("MainActivity", "Password: $password")
+        Log.d("RegisterActivity", "Email is: " + email)
+        Log.d("RegisterActivity", "Password: $password")
 
         //Firebase Authentication to create a user with email and password
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener {
                 if(!it.isSuccessful) return@addOnCompleteListener
 
-                val userId = FirebaseAuth.getInstance().currentUser
-                // 현재유저의 유저아이디와 함께 액티비티를 전환시키는 함수 호출
-                moveMainPage(userId)
-
                 // else if successful
-                Log.d("Main", "Successfully created user with uid: ${it.result!!.user.uid}")
+                Log.d("RegisterActivity", "Successfully created user with uid: ${it.result!!.user.uid}")
+
+                uploadImageToFirebaseStorage()
+                //회원가입 성공시다음페이지로
+                val intent = Intent(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
             }
             .addOnFailureListener{
-                Log.d("Main","Failed to create user: ${it.message}")
+                Log.d("RegisterActivity","Failed to create user: ${it.message}")
                 Toast.makeText(this, "회원가입이 실패되었습니다.: ${it.message}", Toast.LENGTH_SHORT).show()
             }
 
     }
 
-    fun moveMainPage(user: FirebaseUser?) {
-        // 유저가 로그인에 성공하여 메인엑티비티로 넘어가는 부분
-        if(user != null) {
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
+
+    private fun uploadImageToFirebaseStorage(){
+        if(selectedPhotoUri == null) return
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener {
+                Log.d("RegisterActivity","Successfully uploaded image: ${it.metadata?.path}")
+
+                ref.downloadUrl.addOnSuccessListener {
+                    Log.d("RegisterActivity","File Location: $it")
+
+                    //saveUserToFirebaseDatabase(it.toString())
+                }
+            }
+        //.addOnFailureListener {
+        //do some logging here
+        //}
     }
+
+    /*private fun saveUserToFirebaseDatabase(profileImageUrl: String){
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+        val user = User(uid, username_edittext_register.text.toString(), profileImageUrl)
+        ref.setValue(user)
+            .addOnSuccessListener {
+                Log.d("RegisterActivity","Finally we saved the user to Firebase Database")
+                val intent = Intent(this, NextActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+    }*/
 }
